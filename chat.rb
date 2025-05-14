d5ae739b4fbdc88324ac4950a5d2e51001dc8eda
Karen Barbé <info@karenbarbe.com>
2025-05-13 20:22:37 -0500

    create chat.rb

--- chat.rb
+++ chat.rb
@@ -1,1 +1,26 @@
-# Write your solution here!
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+pp client
+
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  },
+  {
+    "role" => "user",
+    "content" => "Hello! What are the best spots for pizza in Chicago?"
+  }
+]
+
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp api_response
